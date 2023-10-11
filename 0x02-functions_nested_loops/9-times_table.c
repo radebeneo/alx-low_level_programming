171931da720a89f6abb2d5ee3e1571bc3fb12858
radebeneo <radebeneo17@gmail.com>
2023-10-11 11:41:58 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * time_table - a function that prints the nine times table
+ */
+
+void time_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i = i + 1)
+	{
+		for (j = 0; j < 10; j = j + 1)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
