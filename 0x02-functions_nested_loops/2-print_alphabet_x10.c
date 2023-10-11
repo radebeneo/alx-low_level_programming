a5623556e6a49b115ad3b11dd13dde83799ec533
radebeneo <radebeneo17@gmail.com>
2023-10-11 09:47:12 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  a function that prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while(i<10)
+	{
+		alpha = 'a';
+
+		while(alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha = alpha + 1;
+		}
+		_putchar('\n');
+		i = i + 1;
+	}
+}
