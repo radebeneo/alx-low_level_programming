7a13505b851426991a29a08c6ed0954437fbeeb2
radebeneo <radebeneo17@gmail.com>
2023-10-11 06:29:57 -0700

    _putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - writes the character c to standard output
  * @c: Character to be printed
  *
- * Return 1
+ * Return: 1
  * Error, returns -1, errno i sset appropriately
  */
 int _putchar(char c)
