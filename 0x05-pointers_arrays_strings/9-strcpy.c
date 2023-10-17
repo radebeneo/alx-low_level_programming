c75942cbe792ccb60c8a618e86c9236faa42defb
radebeneo <radebeneo17@gmail.com>
2023-10-17 09:40:25 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,15 +11,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int j;
+	int i = j = 0;
 
 	while (*(src + i) != '\0')
 	{
 		i++;
 	}
 
-	for (j = 0; j < 1; j++)
+	for ( ; j < 1; j++)
 	{
 		dest[j] = src[j];
 	}
@@ -28,5 +27,3 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
-
-
