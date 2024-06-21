4fbd7c41b172ba32fb5ac1f9303eaaa9c1145710
radebeneo <radebeneo17@gmail.com>
2024-06-20 20:29:13 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints a line using the puts function
+ * Return: 0
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
