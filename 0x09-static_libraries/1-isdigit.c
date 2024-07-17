2e6167c23f439c138fc9f6a6df0b13f88d6e015d
radebeneo <radebeneo17@gmail.com>
2024-07-17 12:39:09 -0700

    1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - verifies if character is a digit
+ * @c: character to be checked
+ * Return: 1 if c is a digit, 0 if not
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
