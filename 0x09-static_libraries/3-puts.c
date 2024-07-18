9bed67c6ff228f8138b14052e81ae3e963d86079
radebeneo <radebeneo17@gmail.com>
2024-07-17 19:25:57 -0700

    3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - point of entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
