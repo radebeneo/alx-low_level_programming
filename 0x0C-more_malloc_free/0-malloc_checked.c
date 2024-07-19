6a5f372b5c714fc3b604b2e3ee9274f77792a7fe
radebeneo <radebeneo17@gmail.com>
2024-07-18 18:44:04 -0700

    Trust no one.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,16 @@
 
 /**
  * malloc_checked - allocates memory using malloc
- * @b: input
+ * @b: input integer
  * Return: void
  */
-void *malloc_checked(unsigned int)
+void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
-	if (ptr ==NULL)
+
+	if (ptr == NULL)
 		exit(98);
 
 	return (ptr);
