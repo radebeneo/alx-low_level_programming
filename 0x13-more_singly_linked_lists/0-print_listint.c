7bc26feb2d5ba913940300096c99daddde957cee
radebeneo <radebeneo17@gmail.com>
2024-08-07 18:14:30 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
-		i
 	}
 
-	return (num);
+	return (nodes);
 }
