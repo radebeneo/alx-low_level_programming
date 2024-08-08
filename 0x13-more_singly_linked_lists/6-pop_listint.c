08cbea6899755c0ea61de653428e8db56bc3d4b0
radebeneo <radebeneo17@gmail.com>
2024-08-07 19:40:09 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,11 +11,11 @@ int pop_listint(listint_t **head)
 	int n;
 	listint_t *tmp;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL || (*head) == NULL)
 		return (0);
 
 	n = (*head)->n;
-	tmp = *head
+	tmp = *head;
 	(*head) = (*head)->next;
 	free(tmp);
 
