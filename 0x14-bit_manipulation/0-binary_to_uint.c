a2a428d3c919fab332691910d2f8430e7f36d2d9
radebeneo <radebeneo17@gmail.com>
2024-08-09 20:32:31 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (a = 0; b[a]; a++)
 	{
-		if (b[a] != '0' || b[a] != '1')
+		if (b[a] < '0' || b[a] > '1')
 			return (0);
 
 		num = 2 * num + (b[i] - '0');
