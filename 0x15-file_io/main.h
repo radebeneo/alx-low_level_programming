40135417bfa51274873cfee452c9139301fb5315
radebeneo <radebeneo17@gmail.com>
2025-01-30 03:58:45 -0800

    Tread lightly, she is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int putchar(char c);
+
